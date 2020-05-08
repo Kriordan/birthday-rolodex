7acce7f40b7160273891fcd44994e382251f2624
Keith Riordan <keith.riordan@gmail.com>
2020-05-08 15:07:15 -0400

    Set up prod firebase environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFyJ4YYXsqAdPiflFrEsJjF8aELOjto2U',
+    authDomain: 'birthday-rolodex-prod.firebaseapp.com',
+    databaseURL: 'https://birthday-rolodex-prod.firebaseio.com',
+    projectId: 'birthday-rolodex-prod',
+    storageBucket: 'birthday-rolodex-prod.appspot.com',
+    messagingSenderId: '110391888302',
+    appId: '1:110391888302:web:1d27b68160ebbe98f4603f',
+    measurementId: 'G-73ERVCX86E',
+  },
 };
